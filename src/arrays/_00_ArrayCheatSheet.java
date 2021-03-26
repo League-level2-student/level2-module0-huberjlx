b482fb60276b3cc991216f4cc9c55ff654048b22
huber <huber@DESKTOP-JF6RRJB>
2021-03-25 20:00:10 -0700

    Halfway done with level 2 mod 0.

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,48 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String [] strings = {"string", "another string", "a third string", "a fourth string", "last string"};
 		//2. print the third element in the array
-		
+		System.out.println(strings[2]);
 		//3. set the third element to a different value
-		
+		strings[2] = "a 3rd string";
 		//4. print the third element again
-		
+		System.out.println(strings[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for (int i = 0; i < strings.length; i ++) {
+			System.out.println(strings[i]);
+		}
 		
 		//6. make an array of 50 integers
-		
+		int [] ints = new int[50];
+
 		//7. use a for loop to make every value of the integer array a random number
-		
+		Random rand = new Random();
+		for (int j = 0; j < ints.length; j++) {
+			ints[j] = rand.nextInt(100);
+		}
 		//8. without printing the entire array, print only the smallest number in the array
-		
+		int minNum = ints[0];
+		int maxNum = ints[0];
+		for (int newInteger : ints) {
+			if (newInteger < minNum) {
+				minNum = newInteger;
+			}
+			else if (newInteger > maxNum) {
+				maxNum = newInteger;
+			}
+		}
+		System.out.println(minNum);
 		//9 print the entire array to see if step 8 was correct
-		
+		for (int nextInteger = 0; nextInteger < ints.length; nextInteger++) {
+			System.out.println(ints[nextInteger]);
+		}
 		//10. print the largest number in the array.
+		System.out.println(maxNum);
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,17 +1,37 @@
 package arrays;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
-
+	public static void main(String[] args) {
+	
 	//2. create an array of 5 robots.
-
+	Robot [] robots = new Robot[5];
 	//3. use a for loop to initialize the robots.
-
+		for (int i = 0; i < robots.length; i++) {
+			robots[i] = new Robot();
 		//4. make each robot start at the bottom of the screen, side by side, facing up
-
+			robots[i].setY(500);
+			robots[i].setSpeed(10);
+			robots[i].setX(100 + (100 * i));
+			robots[i].setAngle(0);
+		}
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
-
+	boolean notAtTop = true;
+	while (notAtTop == true);
+		Random rand = new Random();
+		for (int j = 0; j < robots.length; j++) {
+			int num = rand.nextInt();
+			robots[j].move(num);
+			if (robots[j].getY() <= 0) {
+				notAtTop = false;
+			}
+		}
+	}
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 	//7. declare that robot the winner and throw it a party!
@@ -19,4 +39,7 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
-}
+		
+		
+	}
+
